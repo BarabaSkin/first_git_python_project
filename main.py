89de9825ee1bb97fec6d4f571e69b78d15c9efd2
BarabaSkin <kolyan777.k9@gmail.com>
2022-07-28 16:12:41 +0300

    first version of function, but it can't compare all of values

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("Hello world!")
-print("Another commit")
-print("hi")
+from random import randint
+
+array_with_numbers = []
+array_with_unique_numbers = []
+
+for i in range(100):
+    array_with_numbers.append(randint(1, 5))
+print(*array_with_numbers)
+
+
+def Unique_Values(array, unique_array):
+    unique_array.append(array[0])
+    for i in range(100):
+        for p in range(0, len(unique_array)):
+            if array[i] != unique_array[p]:
+                unique_array.append(array[i])
+            break
+    return unique_array
+
+
+Unique_Values(array_with_numbers, array_with_unique_numbers)
+print(*array_with_unique_numbers)
